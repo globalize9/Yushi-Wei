e291e8b45b87974bb9fac5268e1af2b2b38c26be
Yushi Wei <58374480+globalize9@users.noreply.github.com>
2022-03-20 10:58:08 -0700

    Update stock_call_replacement.py
    replacing stock with calls

--- stock_call_replacement.py
+++ stock_call_replacement.py
@@ -7,6 +7,7 @@ import numpy as np
 from sympy import symbols, solve
 
 def N_d(x):
+    "cumulative normal approx"
     d = [0, 0.0498673470, 0.0211410061, 0.0032776263, 
          0.00000380036, 0.0000488906, 0.0000053830]
     
@@ -19,7 +20,6 @@ def N_d(x):
         approx = result(x)
     if (x<0):
         approx = 1 - result(-x)
-    
     return(approx)
 
 def BS_Form(S0, r, div, sigma, T, X, opt):
@@ -27,41 +27,53 @@ def BS_Form(S0, r, div, sigma, T, X, opt):
     d1 = (np.log(S0 / X) + (r - div + 0.5 * sigma ** 2) * T) / (sigma * np.sqrt(T))
     # d2 = d1 - sigma * np.sqrt(T)
     d2 = (np.log(S0 / X) + (r - div - 0.5 * sigma ** 2) * T) / (sigma * np.sqrt(T))
-    if opt == 'call': opt_price = S0 * N_d(d1) - X * np.exp(-r * T) * N_d(d2)
-    if opt == 'put': opt_price = -S0 * N_d(-d1) + X * np.exp(-r * T) * N_d(-d2)
+    if opt == 'call': opt_price = S0 * np.exp(-div * T) * N_d(d1) - X * np.exp(-r * T) * N_d(d2)
+    if opt == 'put': opt_price = -S0 * np.exp(-div * T) * N_d(-d1) + X * np.exp(-r * T) * N_d(-d2)
     return(opt_price, N_d(d1), N_d(d2))
 
 S0 = 100
 r = 0
 div = 0 
 sigma = 0.2
-T = 0.5
+# T = 0.5
 strike = 100
 call_qty = 2 # optional parameter, otherwise calculate equivalent to 100 shares
 opt_type = 'call'
 
-call_cost = BS_Form(S0, r, div, sigma, T, strike, 'call')
-put_cost = BS_Form(S0, r, div, sigma, T, strike, 'put')
+# call_params = BS_Form(S0, r, div, sigma, T, strike, 'call')
+# put_params = BS_Form(S0, r, div, sigma, T, strike, 'put')
 
 # strategy is to replace long stock position with 2x 50D calls
 # analysis conducted at T
 
-# need to redefine vars
 def UpperBreak(S0, r, div, sigma, T, strike, opt_type, call_qty = None):
     "obtain payoff where long stock == long calls"
     call_cost, delta, prob_itm = BS_Form(S0, r, div, sigma, T, strike, opt_type)
     if call_qty == None: call_qty = round(1 / delta)
     unknown_price = symbols('unknown_price')
     # Solve this eq 
-    expr = (unknown_price - S0) - (unknown_price - strike - call_cost[0]) * call_qty
-    return solve(expr)[0]
+    expr = (unknown_price - S0) - (unknown_price - strike - call_cost) * call_qty
+    return round(solve(expr)[0],4)
 
-UpperBreak(2)
+atm_by_maturity = pd.DataFrame(None, index = [1/12, 3/12, 6/12, 12/12], columns = ['S0','Strike','Delta','Prob_ITM','Upper_Break','Lower_Break', 
+                                                                                   'Opt_Cost_Total', 'Opt_Qty'])
 
-# lower breakpoint is equivalent to call premium
-lower_break = S0 - call_qty * call_cost[0]
+for mat in atm_by_maturity.index:
+    opt_cost, delta, prob_itm = BS_Form(S0, r, div, sigma, mat, strike, opt_type)
+    atm_by_maturity.loc[mat, 'S0'] = S0
+    atm_by_maturity.loc[mat, 'Strike'] = strike
+    atm_by_maturity.loc[mat, 'Opt_Qty'] = call_qty
+    atm_by_maturity.loc[mat, 'Opt_Cost_Total'] = opt_cost * call_qty
+    atm_by_maturity.loc[mat, 'Delta'] = delta
+    atm_by_maturity.loc[mat, 'Prob_ITM'] = prob_itm
+    atm_by_maturity.loc[mat, 'Upper_Break'] = UpperBreak(S0, r, div, sigma, mat, strike, opt_type, call_qty)
+    atm_by_maturity.loc[mat, 'Lower_Break'] = round(S0 - call_qty * opt_cost,4) # lower breakpoint is equivalent to call premium
+    
+atm_by_maturity
 
-atm_by_maturity = pd.DataFrame(None, index = [1/12, 3/12, 6/12, 12/12], columns = ['Strike','Delta','Prob_ITM','Upper_Break','Lower_Break', 'Opt_Cost',
-                                                                                   'Opt_Qty'])
+
+
+
+
 
 
